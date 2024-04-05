61c1f8d0be94762ded06c1d0eb9bed6db6f55c87
William Ndirangu <wwagura0@gmail.com>
2024-04-05 03:33:28 +0300

    Add

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
+function toTitleCase(str) {
+  return str.replace(/\w\S*/g, function (word) {
+    if (word.charAt(0) === word.charAt(0).toUpperCase()) {
+      return word;
+    } else {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }
+  });
+}
+
+function titleCased() {
+  return tutorials.map(toTitleCase);
 }
